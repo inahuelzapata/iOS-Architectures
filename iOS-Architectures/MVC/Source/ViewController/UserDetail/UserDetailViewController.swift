4e8955117423bcb1db33491336bd808635393c9c
Nahuel Zapata <nahuel.zapata@fdvsolutions.com>
2017-12-26 22:29:54 -0300

    Added UserDetailController

---
+++ iOS-Architectures/MVC/Source/ViewController/UserDetail/UserDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UserDetailViewController.swift
+//  iOS-Architectures
+//
+//  Created by Nahuel Zapata on 26/12/2017.
+//  Copyright © 2017 Nahuel Zapata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
